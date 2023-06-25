d85129c017eb9fecb6f5b817335c276c83c6222c
mohamed-ehab51 <modydody512003@gmail.com>
2023-06-26 00:13:34 +0300

    Added splash screen

---
+++ app/src/main/java/com/HANZO/gpa_fcai/splash.java
@@ -0,0 +1,30 @@
+package com.HANZO.gpa_fcai;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+public class splash extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        Window window = this.getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(ContextCompat.getColor(this,R.color.indigo));
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent( splash.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, 2500);
+
+    }
+}
